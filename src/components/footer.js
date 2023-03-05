cc98a71bde12093237089492c4bc077c49ed5b71
Andrea N <andrea@narbot.com>
2023-03-05 16:02:31 -0500

    Adding Stackbit

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,9 +6,7 @@ import * as styles from './footer.module.css'
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
-      Built with changing footer<a href="https://contentful.com/">Contentful</a> and{' '}
-      <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
-      <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
+      Built with <a href="https://www.gatsbyjs.com">Gatsby</a> and <a href="https://www.contentful.com">Contentful</a>
     </div>
   </Container>
 )
---
+++ stackbit.config.js
@@ -0,0 +1,22 @@
+import { ContentfulContentSource } from '@stackbit/cms-contentful';
+import { defineStackbitConfig } from '@stackbit/types';
+/* eslint-disable import/no-anonymous-default-export */ 
+
+
+export default defineStackbitConfig({
+  stackbitVersion: '~0.6.0',
+  ssgName: 'gatsby',
+  nodeVersion: '16',
+  contentSources: [
+    new ContentfulContentSource({
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      environment: process.env.CONTENTFUL_ENVIRONMENT,
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
+    }),
+  ],
+  modelExtensions: [
+    { name: 'page', type: 'page', urlPath: '/{slug}' }
+  ]
+}
+)
